17f716c9341406ee1d2be922e8bbb159cd965e2c
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-08-29 12:57:58 -0500

    Se modificó configuración del server para leer dirección web desde variable de entorno de Heroku

--- 07-restServer/server/config/config.js
+++ 07-restServer/server/config/config.js
@@ -9,7 +9,7 @@ let urlDatabase;
 if (process.env.NODE_ENV === 'dev') {
     urlDatabase = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDatabase = 'mongodb://cafe-user:abc123@ds233228.mlab.com:33228/cafe'
+    urlDatabase = process.env.MONGO_URI
 }
 
 process.env.DB_URL = urlDatabase;
