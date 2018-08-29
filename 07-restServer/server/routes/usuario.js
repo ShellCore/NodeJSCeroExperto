c46694163af4164858adc3c849b75cfea5a9d353
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-08-29 10:33:34 -0500

    Se agregaron validaciones de modificación de usuario en función PUT

--- 07-restServer/server/routes/usuario.js
+++ 07-restServer/server/routes/usuario.js
@@ -5,6 +5,9 @@ const app = express();
 // Encriptación
 const bcrypt = require('bcrypt');
 
+// Underscore (validación de parámetros no editables)
+const _ = require('underscore');
+
 const Usuario = require('../models/usuario');
 
 app.get('/usuario', (req, res) => {
@@ -39,7 +42,38 @@ app.post('/usuario', (req, res) => {
 
 app.put('/usuario/:id', (req, res) => {
     let id = req.params.id;
-    res.json({ id });
+
+    let validEditableFields = [
+        'name',
+        'email',
+        'img',
+        'role',
+        'state'
+    ];
+
+    let body = _.pick(req.body, validEditableFields);
+
+    Usuario.findByIdAndUpdate(
+        id,
+        body, {
+            new: true,
+            runValidators: true
+        },
+        (err, usuarioDB) => {
+            if (err) {
+                return res.status(400)
+                    .json({
+                        ok: false,
+                        err
+                    });
+            }
+            res.json({
+                ok: true,
+                usuario: usuarioDB
+            });
+        });
+
+
 });
 
 app.delete('/usuario', (req, res) => {
