359ed6d4fa26b35abbbe88d333472a6266042d3e
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-08-29 11:50:32 -0500

    Implementación completa de las funciones REST del usuario

--- 07-restServer/server/routes/usuario.js
+++ 07-restServer/server/routes/usuario.js
@@ -11,14 +11,42 @@ const _ = require('underscore');
 const Usuario = require('../models/usuario');
 
 app.get('/usuario', (req, res) => {
-    res.json('get usuario local!');
+
+    let from = req.query.from || 0;
+    from = Number(from);
+
+    let limit = req.query.limit || 5;
+    limit = Number(limit);
+
+    Usuario.find({ state: true }, 'name email state') // {} Aqui se puede meter filtro de validación en campos
+        .skip(from)
+        .limit(limit)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400)
+                    .json({
+                        ok: false,
+                        err
+                    });
+            }
+
+            Usuario.count({ state: true }, (err, conteo) => { // La validación de campos debe ser la mima del query
+                res.json({
+                    ok: true,
+                    usuarios,
+                    count: conteo
+                });
+            });
+
+
+        });
 });
 
 app.post('/usuario', (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
-        name: body.nombre,
+        name: body.name,
         email: body.email,
         password: bcrypt.hashSync(body.password, 10),
         role: body.role
@@ -72,12 +100,66 @@ app.put('/usuario/:id', (req, res) => {
                 usuario: usuarioDB
             });
         });
-
-
 });
 
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario');
+// app.delete('/usuario/:id', function(req, res) {
+//     let id = req.params.id;
+//     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+//         if (err) {
+//             return res.status(400)
+//                 .json({
+//                     ok: false,
+//                     err
+//                 });
+//         }
+
+//         if (!usuarioBorrado) {
+//             return res.status(400)
+//                 .json({
+//                     ok: false,
+//                     err: 'Usuario no encontrado'
+//                 });
+//         }
+
+//         res.json({
+//             ok: true,
+//             usuario: usuarioBorrado
+//         });
+//     });
+// });
+
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    let cambiaEstado = {
+        state: false
+    };
+
+    Usuario.findByIdAndUpdate(
+        id,
+        cambiaEstado, { new: true },
+        (err, usuarioBorrado) => {
+            if (err) {
+                return res.status(400)
+                    .json({
+                        ok: false,
+                        err
+                    });
+            }
+
+            if (!usuarioBorrado) {
+                return res.status(400)
+                    .json({
+                        ok: false,
+                        err: 'Usuario no encontrado'
+                    });
+            }
+
+            res.json({
+                ok: true,
+                usuario: usuarioBorrado
+            });
+        });
 });
 
 module.exports = app;
