a387588b5d8152ed7b2612313e74d9235ddeac0d
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-01 15:16:40 -0500

    Realización del código para levantamiento y seguimiento de tickets

--- 08-socket-basico/server/sockets/socket.js
+++ 08-socket-basico/server/sockets/socket.js
@@ -1,31 +1,32 @@
 const { io } = require('../server');
+const { TicketControl } = require('../classes/ticket-control');
+
+const ticketControl = new TicketControl();
 
 io.on('connection', (client) => {
-    console.log('Usuario conectado');
 
-    client.emit('enviarMensaje', {
-        usuario: 'Admin',
-        mensaje: 'Bienvenido a ésta aplicación'
+    client.on('siguienteTicket', (data, callback) => {
+        let siguiente = ticketControl.siguiente();
+        console.log(siguiente);
+
+        callback(siguiente);
     });
 
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
+    client.emit('estadoActual', {
+        actual: ticketControl.getUltimoTicket()
     });
 
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
-        console.log(data);
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
 
-        client.broadcast.emit('enviarMensaje', data);
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+        callback(atenderTicket);
 
-        // if (message.usuario) {
-        //     callback({
-        //         respuesta: 'Todo salió bien'
-        //     });
-        // } else {
-        //     callback({
-        //         respuesta: 'Todo salió mal'
-        //     });
-        // }
+
     });
 });
