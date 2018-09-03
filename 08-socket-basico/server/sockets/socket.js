0ea1c376624c34658c6bfbb5e088dfdcc6e059e8
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-03 15:24:24 -0500

    Se agregó funcionalidad en la vista de tickets atendidos

--- 08-socket-basico/server/sockets/socket.js
+++ 08-socket-basico/server/sockets/socket.js
@@ -13,7 +13,8 @@ io.on('connection', (client) => {
     });
 
     client.emit('estadoActual', {
-        actual: ticketControl.getUltimoTicket()
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
     });
 
     client.on('atenderTicket', (data, callback) => {
@@ -27,6 +28,8 @@ io.on('connection', (client) => {
         let atenderTicket = ticketControl.atenderTicket(data.escritorio);
         callback(atenderTicket);
 
-
+        client.broadcast.emit('ultimos4', {
+            ultimos4: ticketControl.getUltimos4();
+        })
     });
 });
