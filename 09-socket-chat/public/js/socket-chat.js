bd7ed3f1024740d926822d14ec10a1bf218442ae
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-03 17:14:02 -0500

    Se actualizó vista de chat y vista index

--- 09-socket-chat/public/js/socket-chat.js
+++ 09-socket-chat/public/js/socket-chat.js
@@ -1,27 +1,43 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y/o sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
         console.log('Usuarios conectados', resp);
     });
+
 });
 
 // escuchar
 socket.on('disconnect', function() {
+
     console.log('Perdimos conexión con el servidor');
+
 });
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
@@ -30,11 +46,13 @@ socket.on('crearMensaje', function(mensaje) {
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-socket.on('listaPersonas', function(personas) {
+socket.on('listaPersona', function(personas) {
     console.log(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
