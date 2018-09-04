26469927b7b12db596378989666635e08ce37f46
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-04 17:34:35 -0500

    Se agregaron queries a la base de datos

--- 10-typescript-mysql/src/mysql/mysql.ts
+++ 10-typescript-mysql/src/mysql/mysql.ts
@@ -19,6 +19,26 @@ export default class MySQL {
         this.conectarDb();
     }
 
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    static ejecutarQuery(query: string, callback: Function) {
+        this.instance.connection.query(query, (err, results:Object[], fields) => {
+            if (err) {
+                console.log('Error en query');
+                console.log(err);
+                return callback(err);
+            }
+
+            if (results.length === 0) {
+                callback('El registro solicitado no existe');
+            } else {
+                callback(null, results);
+            }
+        });
+    }
+
     private conectarDb() {
         this.connection.connect((err: mysql.MysqlError) => {
             if (err) {
--- 10-typescript-mysql/src/router/router.ts
+++ 10-typescript-mysql/src/router/router.ts
@@ -1,22 +1,49 @@
 import {Router, Request, Response} from 'express';
+import MySQL from '../mysql/mysql';
 
 const router = Router();
 
 router.get('/heroes', (req: Request, res: Response) => {
-    res.json({
-        ok : true,
-        mensaje: 'Todo está bien'
+    
+    const query = `SELECT * FROM heroes`;
+
+    MySQL.ejecutarQuery(query, (err: any, heroes: Object[]) => {
+        if (err) {
+            res.status(400)
+            .json({
+                ok : false,
+                error : err
+            });
+        } else {
+            res.json({
+                ok : true,
+                heroes
+            });
+        }
     });
 });
 
 router.get('/heroes/:id', (req: Request, res: Response) => {
     
     const id = req.params.id;
-    
-    res.json({
-        ok : true,
-        mensaje: 'Todo está bien',
-        id
+    const idEscaped = MySQL.instance.connection.escape(id);
+
+
+    const query = `SELECT * FROM heroes WHERE id = ${idEscaped}`;
+
+    MySQL.ejecutarQuery(query, (err: any, heroes: Object[]) => {
+        if (err) {
+            res.status(400)
+            .json({
+                ok : false,
+                error : err
+            });
+        } else {
+            res.json({
+                ok : true,
+                heroe : heroes[0]
+            });
+        }
     });
 });
 
